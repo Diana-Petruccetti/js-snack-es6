403b4764f3eb2f55b7a9337a373e77f718bdf9ce
Diana-Petruccetti <dianapetruccetti@gmail.com>
2024-10-11 15:53:48 +0200

    created array

--- Snack-1/assets/js/main.js
+++ Snack-1/assets/js/main.js
@@ -2,3 +2,27 @@
 Creare un array di oggetti:
 Ogni oggetto descriverà una bici da corsa con le seguenti proprietà: nome e peso.
 Stampare a schermo la bici con peso minore. */
+
+// 1 Raccolta Dati
+// Scrivere array di oggetti biciclette
+const biciclette = [
+    {
+        "Nome": "Bici 1",
+        "Peso": 10,
+    },
+    {
+        "Nome": "Bici 2",
+        "Peso": 8,
+    },
+    {
+        "Nome": "Bici 3",
+        "Peso": 4,
+    },
+    {
+        "Nome": "Bici 1",
+        "Peso": 6,
+    },
+]
+
+// 2 Elaborazione Dati
+// Creare condizione if per stampare peso più piccolo in console
