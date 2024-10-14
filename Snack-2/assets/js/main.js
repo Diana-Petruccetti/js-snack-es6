704dc76bce8ae5102bd64011985f6a99a0ee301a
Diana-Petruccetti <dianapetruccetti@gmail.com>
2024-10-14 10:22:36 +0200

    snack 2 completed

--- Snack-2/assets/js/main.js
+++ Snack-2/assets/js/main.js
@@ -4,3 +4,47 @@ Ogni squadra avrà diverse proprietà: nome, punti fatti, falli subiti.
 Nome sarà l’unica proprietà da compilare, le altre saranno tutte settate a 0.
 Generare numeri random al posto degli 0 nelle proprietà “punti” fatti e “falli subiti”.
 Infine, creiamo un nuovo array i cui elementi contengono solo nomi e falli subiti e stampiamo tutto in console. */
+
+function getRandomNumbers(min, max){
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+const teams = [
+    {
+        name: 'Team 1',
+        points: Math.ceil(Math.random() * 10),
+        fouls: Math.ceil(Math.random() * 10)
+    },
+    {
+        name: 'Team 2',
+        points: getRandomNumbers(1, 10),
+        fouls: getRandomNumbers(1, 10),
+    },
+    {
+        name: 'Team 3',
+        points: getRandomNumbers(1, 10),
+        fouls: getRandomNumbers(1, 10),
+    },
+    {
+        name: 'Team 4',
+        points: getRandomNumbers(1, 10),
+        fouls: getRandomNumbers(1, 10),
+    }
+]
+
+console.log(teams);
+
+const newTeams = [];
+for (let i = 0; i < teams.length; i++){
+    const team = teams[i]
+
+    console.log(team.name);
+    console.log(team.fouls);
+
+    newTeams.push({
+        name: team.name,
+        fouls: team.fouls,
+    })
+}
+
+console.log(newTeams, teams);
